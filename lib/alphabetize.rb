d05d7da2b36b5486b04e499d0d8cf0ceffa6ce6d
Rylan Bauermeister <rylan@bauermeister.com>
2019-03-01 01:02:22 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+<<<<<<< HEAD
   arr.sort{|word1, word2| 
     result = 0
     for i in 0...word1.length
@@ -13,4 +14,26 @@ def alphabetize(arr)
     result
   }
   
+=======
+    binding.pry
+  result = arr.sort{|phrase1, phrase2|
+    for i in 0...phrase1.length
+      if alphabet.index(phrase1[i]) > alphabet.index(phrase2[i])
+        return 1
+      elsif alphabet.index(phrase1[i]) < alphabet.index(phrase2[i])
+        return -1
+      end
+    end
+    binding.pry
+    if phrase1.length == phrase2.length
+      return 0
+    elsif phrase1.length > phrase2.length
+      return -1
+    else 
+      return 1
+    end
+  }
+  binding.pry
+  result
+>>>>>>> 3421d43e17c7d3647de4ddb2855007b2d9b3b110
 end
